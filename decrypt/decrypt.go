37e7ab1741fc5217f53583a765e71cca9dcd1954
15654821354365456 <101603039+julianassange1@users.noreply.github.com>
2022-03-15 01:30:48 +0300

    a

---
+++ decrypt/decrypt.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"io/ioutil"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
+)
+
+func main() {
+	say, err := cowsay.Say(
+		"...than it is to decrypt it.",
+		cowsay.Type("bud-frogs"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
+	fmt.Println("Decryption")
+	fmt.Println()
+
+	SecretKey := []byte("freepressfreeejulianassange2006!")
+	ciphertext, err := ioutil.ReadFile("../encrypt/a.data")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	c, err := aes.NewCipher(SecretKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		fmt.Println(err)
+	}
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(plaintext))
+}
---
+++ encrypt/encrypt.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
+)
+
+func main() {
+	say, err := cowsay.Say(
+		"It is easier to encrypt information...",
+		cowsay.Type("bud-frogs"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
+	fmt.Println("Encryption")
+	fmt.Println()
+
+	text := []byte("Hello")
+	SecretKey := []byte("freepressfreeejulianassange2006!")
+
+	c, err := aes.NewCipher(SecretKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+	err = ioutil.WriteFile("a.data", gcm.Seal(nonce, nonce, text, nil), 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
